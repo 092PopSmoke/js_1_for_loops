fb6cb78f2fb1625242e7a7085ebda82789a003f6
Lean <93644347+092PopSmoke@users.noreply.github.com>
2021-12-08 10:06:20 +0100

    Add files via upload

---
+++ loops4.js
@@ -0,0 +1,12 @@
+var a = window.prompt("Skrive en tekst.");
+
+for (var i = 6; i > 0; i--) {
+    document.write("<h"+i+">"+a+"</h"+i+">");
+    console.log(i);
+}
+
+// for (var i = 0; i < 7M i++) 
+// {
+// document.write("<span style='f
+// }
+// fra serdar
